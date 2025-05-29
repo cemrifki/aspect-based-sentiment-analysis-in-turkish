95ae5b1ae32f4e3be01351c57c276f3031fa99e0
cemrifki <cemrifkiaydin@gmail.com>
2025-05-29 20:33:12 +0300

    Some minor errors are tweaked as well.

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 SBERT_MODEL_NAME = 'all-MiniLM-L6-v2'
 ASPECT_COUNT = 5
-INPUT_PATH = "clean_data.csv"  # The data is received from: https://www.kaggle.com/code/nkitgupta/aspect-based-sentiment-analysis/notebook
+INPUT_PATH = "clean_data.csv"  # The data is received from: https://www.kaggle.com/code/nkitgupta/aspect-based-sentiment-analysis/
 
