224b799fec2eec30124cba069e809206718a1122
cemrifki <cemrifkiaydin@gmail.com>
2025-05-29 21:04:16 +0300

    Some comments are added to the file constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,7 @@
+# The name of the Sentence Transformer model
 SBERT_MODEL_NAME = 'all-MiniLM-L6-v2'
+# The overall aspect count for the whole corpus (specific to a domain)
 ASPECT_COUNT = 5
-INPUT_PATH = "clean_data.csv"  # The data is received from: https://www.kaggle.com/code/nkitgupta/aspect-based-sentiment-analysis/
+# The data is received from: https://www.kaggle.com/code/nkitgupta/aspect-based-sentiment-analysis/
+INPUT_PATH = "clean_data.csv"  
 
